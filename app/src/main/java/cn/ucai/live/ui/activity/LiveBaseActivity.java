6215f6f6777bb93622814651462045cd22e1bf62
juheng <1624371584@qq.com>
2017-03-03 15:19:30 +0800

    打赏礼物列表的自定义View

--- app/src/main/java/cn/ucai/live/ui/activity/LiveBaseActivity.java
+++ app/src/main/java/cn/ucai/live/ui/activity/LiveBaseActivity.java
@@ -440,6 +440,13 @@ public abstract class LiveBaseActivity extends BaseActivity {
   }
 
   @OnClick(R.id.present_image) void onPresentImageClick() {
+    final RoomGiftListDialog dialog =
+            RoomGiftListDialog.newInstance();
+    dialog.show(getSupportFragmentManager(), "RoomUserDetailsDialog");
+    //sendGiftMsg();
+  }
+
+  private void sendGiftMsg() {
     EMMessage message = EMMessage.createSendMessage(EMMessage.Type.CMD);
     message.setReceipt(chatroomId);
     EMCmdMessageBody cmdMessageBody = new EMCmdMessageBody(LiveConstants.CMD_GIFT);
